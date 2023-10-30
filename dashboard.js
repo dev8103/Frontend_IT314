dd1f4e50dbde8f7880d90a43939021acaa064cf0
yash_panasara <yashpansara02@gmail.com>
2023-10-30 21:34:20 +0530

    Dashboard changes

--- dashboard.js
+++ dashboard.js
@@ -1,28 +1,6 @@
-'use strict';
+let menuicn = document.querySelector(".menuicn");
+let nav = document.querySelector(".navcontainer");
 
-const menu = document.querySelector('.menu');
-const close= document.querySelector('.close');
-
-menu.addEventListener('click',function(){
-    const box1=document.querySelector('.box1');
-
-    console.log('menu');
-
-        box1.style.display='block';
-        close.style.display='block';
-        menu.style.display='none';
-
-
-});
-
-close.addEventListener('click',function(){
-    const box1=document.querySelector('.box1');
-
-    console.log('close');
-
-
-    box1.style.display='none';
-    close.style.display='none';
-    menu.style.display='block';
-
-});
+menuicn.addEventListener("click", () => {
+	nav.classList.toggle("navclose");
+})
