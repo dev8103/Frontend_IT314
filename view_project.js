1cddec58ad3e244cff7075ca261c8a031c39eff2
Hitesh Rathva <hiteshrathva9099@gmail.com>
2023-11-04 00:28:24 +0530

    updated view_project

--- view_project.js
+++ view_project.js
@@ -4,3 +4,19 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => {
     nav.classList.toggle("navclose");
 })
+
+const btn=document.querySelector(".button-tag");
+const descbox=document.querySelector(".description-box");
+
+btn.onclick=function() {showtask()};
+
+function showtask(){
+    if(btn.textContent=="view"){
+    descbox.style.display="flex";
+    btn.textContent="Hide";
+    }
+    else{
+    descbox.style.display="none";
+    btn.textContent="view"; 
+    }
+}
