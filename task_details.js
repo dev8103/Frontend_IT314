a498694a68d0fff9f0ef4272ed474b1d853b8540
Hitesh Rathva <hiteshrathva9099@gmail.com>
2023-11-25 10:09:28 +0530

    updated task details

---
+++ task_details.js
@@ -0,0 +1,6 @@
+if(document.querySelector("#statusdetail").textContent=="In Progress"){
+    document.querySelector(".submit-container").style.display="flex";
+ }
+ else{
+   document.querySelector(".submit-container").style.display="none";
+ }
