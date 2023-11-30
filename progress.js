bc9530352df251f65589f9e81f1719138a47e03c
narendra thakkar <107904144+Spartan452@users.noreply.github.com>
2023-11-30 23:04:28 +0530

    Update progress.js

--- progress.js
+++ progress.js
@@ -4,3 +4,17 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => {
     nav.classList.toggle("navclose");
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the progress element
+    var progressElement = document.querySelector(".progress");
+
+    // Get the value of variable i
+    var iValue = parseFloat(progressElement.style.getPropertyValue("--i"));
+
+    // Check if iValue is less than 50
+    if (iValue < 50) {
+        // If less than 50, add the "less" class
+        progressElement.classList.add("less");
+    }
+});
